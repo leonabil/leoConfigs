0393aa36dc88e05685dcb66c04575f9265c12c93
Leo Nabil <leo-subscriptions@pm.me>
2025-02-21 23:53:03 +0000

    Sorting this mess out

---
+++ linuxWall/nerdfonts.sh
@@ -0,0 +1,78 @@
+declare -a fonts=(
+    0xProto
+    3270
+    Agave
+    AnonymousPro
+    Arimo
+    AurulentSansMono
+    BigBlueTerminal
+    BitstreamVeraSansMono
+    IBMPlexMono
+    CascadiaCode
+    CascadiaMono
+    CodeNewRoman
+    ComicShannsMono
+    CommitMono
+    Cousine
+    D2Coding
+    DaddyTimeMono
+    DepartureMOno
+    DejaVuSansMono
+    DroidSansMono
+    EnvyCodeR
+    FantasqueSansMono
+    FiraCode
+    FiraMono
+    GeistMono    
+    Go-Mono
+    Gohu
+    Hack
+    Hasklig
+    HeavyData
+    Hermit
+    iA-Writer
+    Inconsolata
+    InconsolataGo
+    InconsolataLGC
+    IntelOneMono
+    Iosevka
+    IosevkaTerm
+    IosevkaTermSlab
+    JetBrainsMono
+    Lekton
+    LiberationMono
+    Lilex
+    MartianMono
+    Meslo
+    Noto
+    Monaspace
+    Monofur
+    Monoid
+    Mononoki
+    MPlus
+    Noto
+    OpenDiyslexic
+    Overpass
+    ProFont
+    ProggyClean
+    Recursive
+    RobotoMono
+    ShareTechMono
+    SourceCodePro
+    SpaceMono
+    NerdFontsSymbolsOnly
+    Terminus
+    Tinos
+    Ubuntu
+    UbuntuMono
+    UbuntuSans
+    VictorMono
+    ZedMOno
+)
+version='3.3.0'
+fonts_dir="${HOME}/.local/share/fonts"
+if [[ ! -d "$fonts_dir" ]]; then
+    mkdir -p "$fonts_dir"
+fi
+find "$fonts_dir" -name '*Windows Compatible*' -delete
+fc-cache -fv
